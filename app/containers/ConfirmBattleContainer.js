4b35923d77f3f336dc2079daf36a23d5d58f542e
Leo Lee <nxl1969@gmail.com>
2017-01-18 00:48:52 -0500

    modified:   app/components/ConfirmBattle.js 	new file:   app/components/UserDetails.js 	new file:   app/components/UserDetailsWrapper.js 	modified:   app/containers/ConfirmBattleContainer.js 	modified:   app/styles/index.js 	new file:   app/utils/githubHelpers.js 	modified:   package.json

--- app/containers/ConfirmBattleContainer.js
+++ app/containers/ConfirmBattleContainer.js
@@ -1,6 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
-
+var githubHelpers = require('../utils/githubHelpers');
 var ConfirmBattleContainer = React.createClass({
 	contextTypes: {
 		router: React.PropTypes.object.isRequired
@@ -14,11 +14,30 @@ var ConfirmBattleContainer = React.createClass({
 	componentWillMount:function(){
 		
 	},
+	handleInitiateBattle:function(){
+		this.context.router.push({
+			pathname: '/results',
+			state:{
+				playersInfo:this.state.playersInfo
+			}
+		});	
+	},
 	render:function(){
-		return <ConfirmBattle isLoading = {this.state.isLoading} playersInfo={this.state.playersInfo}/>
+		return <ConfirmBattle 
+		isLoading = {this.state.isLoading} 
+		playersInfo={this.state.playersInfo}
+		onInitiateBattle = {this.handleInitiateBattle}/>
 	},
 	componentDidMount:function(){
 		var query = this.props.location.query;
+		githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+		.then(function(players){
+			//console.log('PLAYERS',players);
+			this.setState({
+				isLoading:false,
+				playersInfo:[players[0],players[1]]
+			})
+		}.bind(this));
 	},
 	componentWillReceiveProps:function(){
 	
